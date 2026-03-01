ca51c89f2f9df5d09ff7ac46face10bbbd252af3
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-01 13:18:00 -0300

    refactor: add validation annotations on DTO 'AddNewBookRequest'

--- src/main/java/com/centroweg/pablo/librarysystem/app/dto/AddNewBookRequest.java
+++ src/main/java/com/centroweg/pablo/librarysystem/app/dto/AddNewBookRequest.java
@@ -1,9 +1,38 @@
 package com.centroweg.pablo.librarysystem.app.dto;
 
 import com.centroweg.pablo.librarysystem.domain.BookStatus;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.URL;
 
 public record AddNewBookRequest(
-        BookRequest bookRequest,
+
+        @NotBlank
+        @JsonProperty("isbn")
+        String isbn,
+
+        @NotBlank
+        @JsonProperty("title")
+        String title,
+
+        @NotBlank
+        @JsonProperty("authors")
+        String authors,
+
+        @NotBlank
+        @URL
+        @JsonProperty("thumbnail_url")
+        String thumbnailUrl,
+
+        @NotBlank
+        @JsonProperty("status")
         BookStatus status,
+
+        @NotBlank
+        @Min(1)
+        @Max(5)
+        @JsonProperty("rating")
         int rating
 ) { }
