60be840aa8267244af4bdcfa07e71fb0a4b25320
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-01 13:21:55 -0300

    refactor: add JSON name fields on DTO 'UserResponse'

--- src/main/java/com/centroweg/pablo/librarysystem/app/dto/UserResponse.java
+++ src/main/java/com/centroweg/pablo/librarysystem/app/dto/UserResponse.java
@@ -1,7 +1,15 @@
 package com.centroweg.pablo.librarysystem.app.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record UserResponse(
+
+        @JsonProperty("id")
         Long id,
+
+        @JsonProperty("name")
         String name,
+
+        @JsonProperty("email")
         String email
 ) { }
