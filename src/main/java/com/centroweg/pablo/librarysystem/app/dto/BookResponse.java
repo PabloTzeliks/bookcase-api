a05bd035dc80794bb31d59552b596cf0a54cbf31
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-01 13:20:02 -0300

    refactor: add JSON name fields on DTO 'BookResponse'

--- src/main/java/com/centroweg/pablo/librarysystem/app/dto/BookResponse.java
+++ src/main/java/com/centroweg/pablo/librarysystem/app/dto/BookResponse.java
@@ -1,13 +1,28 @@
 package com.centroweg.pablo.librarysystem.app.dto;
 
 import com.centroweg.pablo.librarysystem.domain.BookStatus;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record BookResponse(
+
+        @JsonProperty("id")
         Long id,
+
+        @JsonProperty("isbn")
         String isbn,
+
+        @JsonProperty("title")
         String title,
+
+        @JsonProperty("authors")
         String authors,
+
+        @JsonProperty("thumbnail_url")
         String thumbnailUrl,
+
+        @JsonProperty("status")
         BookStatus status,
+
+        @JsonProperty("rating")
         int rating
 ) { }
