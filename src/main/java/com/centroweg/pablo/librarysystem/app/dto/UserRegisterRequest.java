6994f6528a57bfd6dbc6dcb318a846b398c00a4c
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-01 13:21:18 -0300

    refactor: add validation annotations on DTO 'UserRegisterRequest'

--- src/main/java/com/centroweg/pablo/librarysystem/app/dto/UserRegisterRequest.java
+++ src/main/java/com/centroweg/pablo/librarysystem/app/dto/UserRegisterRequest.java
@@ -1,7 +1,21 @@
 package com.centroweg.pablo.librarysystem.app.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record UserRegisterRequest(
+
+        @NotBlank
+        @JsonProperty("name")
         String name,
+
+        @NotBlank
+        @Email
+        @JsonProperty("email")
         String email,
+
+        @NotBlank
+        @JsonProperty("password")
         String password
 ) { }
