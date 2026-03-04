6dd20ab7c91ee1ea37e1cd1c720c503027549660
PabloTzeliks <arq.pabloo@gmail.com>
2026-03-04 18:53:56 -0300

    feat: implement configurations for applying the app on an Apache Tomcat web server

--- src/main/java/com/centroweg/pablo/librarysystem/LibrarysystemApplication.java
+++ src/main/java/com/centroweg/pablo/librarysystem/LibrarysystemApplication.java
@@ -2,9 +2,16 @@ package com.centroweg.pablo.librarysystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LibrarysystemApplication {
+public class LibrarysystemApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LibrarysystemApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibrarysystemApplication.class, args);
